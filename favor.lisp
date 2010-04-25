ad6bb2444ba6c354462545d207cbace99fb84743
Kat Marchán <kzm@sykosomatic.org>
2010-04-25 12:03:21 -0500

    reorganized and documented code.

--- favor.lisp
+++ favor.lisp
@@ -149,9 +149,10 @@
 ;;;    to how much that group as a whole likes you, even if other groups have a completely
 ;;;    different opinion of you.
 ;;;
-(defvar *all-pcs* '())
-(defvar *all-transactions* '())
 
+;;;
+;;; Time utils
+;;;
 (defstruct (time (:constructor %make-time
                                (universal-time second minute
                                 hour date month year day daylight-p zone)))
@@ -162,14 +163,22 @@
       (decode-universal-time universal-time)
     (%make-time universal-time second minute hour date month year day daylight-p zone)))
 
+;;;
+;;; Players
+;;;
+(defvar *all-pcs* '()
+  "A list of all PCs (player characters) in the system.")
+
 (defclass pc ()
   ((name :initarg :name
          :initform (error "PC must be initialized with a name."))))
+
 (defmethod initialize-instance :after ((pc pc) &key)
   (if (find (slot-value pc 'name) *all-pcs* :key (lambda (pc) (slot-value pc 'name))
             :test #'string-equal)
       (error "PC already exists!")
       (push pc *all-pcs*)))
+
 (defmethod print-object ((obj pc) stream)
   (print-unreadable-object (obj stream :type t :identity t)
     (format stream "~A" (slot-value obj 'name))))
@@ -177,6 +186,12 @@
 (defun find-pc (name)
   (find name *all-pcs* :key (lambda (pc) (slot-value pc 'name))
         :test #'string-equal))
+
+;;;
+;;; Transactions
+;;;
+(defvar *all-transactions* '()
+  "A list of all transactions in the system.")
 
 (defclass transaction ()
   ((time :initform (make-time)
@@ -204,45 +219,24 @@
   (:method ((obj t)) nil)
   (:method ((obj @disfavor)) t))
 
-(defgeneric global-favor (pc from-date to-date)
-  (:documentation "Returns the global favor accumulated by PC between FROM-DATE and TO-DATE."))
-
-(defgeneric right-handed-favor (observer specimen from-date to-date)
-  (:documentation "Returns the relative favor for SPECIMEN as seen by OBSERVER,
- between FROM-DATE and TO-DATE."))
-(defgeneric left-handed-favor (observer specimen from to))
-
-(defgeneric @favor (actor target &optional description)
-  (:method ((actor pc) (target pc) &optional (description "No description."))
-    (if (eq actor target)
-        (error "No, you can't @favor yourself!")
-        (push (make-instance '@favor
-                             :description description
-                             :source actor
-                             :target target)
-              *all-transactions*))))
-
-(defgeneric @disfavor (actor target &optional description)
-  (:method ((actor pc) (target pc) &optional (description "No description."))
-    (if (eq actor target)
-        (error "No, you can't @disfavor yourself!")
-        (push (make-instance '@disfavor
-                             :description description
-                             :source actor
-                             :target target)
-              *all-transactions*))))
-
+;;;
+;;; Path-finding
+;;;
 (defun node-neighbors (node inclusion-function)
+  "Returns a list of neighbors of NODE. A PC is a neighbor iff there is a transaction from node to
+that PC, and the transaction passes INCLUSION-FUNCTION. INCLUSION-FUNCTION should be a two-argument
+function that accepts a PC (the current node we're getting neighbors for), and a transaction object,
+and returns a generalized boolean that answers whether that transaction's target should be in the
+list of NODE's neighbors."
   (remove-duplicates
    (mapcar (lambda (txn) (slot-value txn 'target))
            (remove-if-not (lambda (txn)
                             (funcall inclusion-function node txn))
                           *all-transactions*))))
 
-(defun relevant-time-p (time upper-bound lower-bound)
-  (apply #'> (mapcar #'time-universal-time (list upper-bound time lower-bound))))
-
 (defun dijkstra (graph source inclusion-func)
+  "Mostly standard implementation of Dijkstra's algorithm. Returns a hash table of distances and
+a hash table with shortest paths. INCLUSION-FUNC is used by #'NODE-NEIGHBORS."
   (let ((distances (make-hash-table :test 'eq))
         (previous (make-hash-table :test 'eq)))
 
@@ -284,6 +278,8 @@
       (values distances previous))))
 
 (defun shortest-indirect-path (graph source target inclusion-func)
+  "Finds the shortest *INDIRECT* path between SOURCE and TARGET. That is, SOURCE->TARGET is not
+considered a valid path."
   (multiple-value-bind (distances previous)
       (dijkstra graph source inclusion-func)
     (declare (ignore distances))
@@ -295,15 +291,64 @@
        finally (return (when list (cons source list))))))
 
 (defun all-indirect-paths (graph source target inclusion-func)
+  "Finds all indirect shortest paths between SOURCE and TARGET."
   (loop for shortest = (shortest-indirect-path graph source target inclusion-func)
      while shortest
      do (setf graph (remove (car (last (butlast shortest))) graph))
      collect shortest))
 
+;;;
+;;; Metrics
+;;;
+
+;;; Generics
+(defgeneric global-favor (pc from-date to-date)
+  (:documentation "Returns the global favor accumulated by PC between FROM and TO."))
+
+(defgeneric right-handed-favor (observer specimen from to)
+  (:documentation "Returns the relative favor for SPECIMEN as seen by OBSERVER, between FROM and
+TO. Right-handed favor is a measurement of what those that you have positive opinions of, and their
+positive connections, think of SPECIMEN."))
+
+(defgeneric left-handed-favor (observer specimen from to)
+  (:documentation "Retuns the left-handed favor for SPECIMEN as seen by OBSERVER, between FROM and
+TO. Left-handed favor is a measurement of what those that you have negative opinions of, and their
+positive connections, think of SPECIMEN."))
+
+(defgeneric @favor (actor target &optional description)
+  (:documentation "Registers one favor from ACTOR to TARGET. Accepts an optional description of why
+it was granted.")
+  (:method ((actor pc) (target pc) &optional (description "No description."))
+    (if (eq actor target)
+        (error "No, you can't @favor yourself!")
+        (push (make-instance '@favor
+                             :description description
+                             :source actor
+                             :target target)
+              *all-transactions*))))
+
+(defgeneric @disfavor (actor target &optional description)
+  (:documentation "Registers one disfavor from ACTOR to TARGET. Accepts an optional description of
+why it was granted.")
+  (:method ((actor pc) (target pc) &optional (description "No description."))
+    (if (eq actor target)
+        (error "No, you can't @disfavor yourself!")
+        (push (make-instance '@disfavor
+                             :description description
+                             :source actor
+                             :target target)
+              *all-transactions*))))
+
+;;; Implementations
 (defparameter *distance-decay-factor* 1)
+(defparameter *repeated-favor-decay* 1/2)
+
 (defun path-favor (path transaction-decay)
+  "Given a path, calculates its total value. TRANSACTION-DECAY measures how quickly repeated
+favor/disfavors decay in value."
   (if (or (null path) (= 1 (length path)))
       (error "Invalid path ~S" path)
+      ;; We only care about the actual opinion of the second-to-last node in the path.
       (let ((judge (car (last (butlast path))))
             (target (car (last path))))
         (let ((unweighted (direct-favor judge target transaction-decay))
@@ -311,6 +356,9 @@
           (/ unweighted weight)))))
 
 (defun direct-favor (judge target decay-factor)
+  "Calculates the direct favor from JUDGE to TARGET. DECAY-FACTOR represents how quickly repeated
+favor/disfavors might decay in value. When DECAY-FACTOR < 1, an infinite number of transactions will
+eventually converge on a single number. When > 1, favor can grow unbounded into infinity."
   (flet ((relevant-transaction-p (transaction)
            (when (and (eq (slot-value transaction 'target) target)
                       (eq (slot-value transaction 'source) judge))
@@ -327,14 +375,15 @@
        (disfavor-value (geometric-sum 1 decay-factor (length disfavors))))
       (- favor-value disfavor-value))))
 
-(defun clamp-transactions (txn-list from to)
-  (remove-if-not (lambda (txn)
-                   (> (time-universal-time to)
-                      (time-universal-time (slot-value txn 'time))
-                      (time-universal-time from)))
-                 txn-list))
+(defun relevant-time-p (time upper-bound lower-bound)
+  "Evaluates to true if TIME is between UPPER-BOUND and LOWER-BOUND."
+  (apply #'> (mapcar #'time-universal-time (list upper-bound time lower-bound))))
 
-(defparameter *decay* 1/2)
+(defun clamp-transactions (txn-list from to)
+  "Returns a list of transactions from TXN-LIST whose time is between FROM and TO."
+  (remove-if-not (lambda (txn)
+                   (relevant-time-p (slot-value txn 'time) to from))
+                 txn-list))
 
 (defmethod global-favor ((pc pc) (from-date time) (to-date time))
   (let* ((*all-transactions* (clamp-transactions *all-transactions* from-date to-date))
@@ -345,7 +394,7 @@
     (reduce #'+ (mapcar (lambda (txn)
                           (direct-favor (slot-value txn 'source)
                                         pc
-                                        *decay*))
+                                        *repeated-favor-decay*))
                         (remove-duplicates relevant-transactions
                                            :key (lambda (txn)
                                                   (slot-value txn 'source)))))))
@@ -353,7 +402,7 @@
 (defun relative-favor (observer specimen from to inclusion-function)
   (let ((*all-transactions* (clamp-transactions *all-transactions* from to)))
     ;; To include someone's direct favor, (cons (direct-favor observer specimen 1/2 (mapcar ... )))
-    (reduce #'+ (mapcar (lambda (path) (path-favor path *decay*))
+    (reduce #'+ (mapcar (lambda (path) (path-favor path *repeated-favor-decay*))
                         (print (all-indirect-paths *all-pcs* observer specimen
                                                    inclusion-function))))))
 
@@ -363,7 +412,7 @@
                     (and (eq node (slot-value txn 'source))
                          (if (eq specimen (slot-value txn 'target))
                              t
-                             (plusp (direct-favor node (slot-value txn 'target) *decay*)))
+                             (plusp (direct-favor node (slot-value txn 'target) *repeated-favor-decay*)))
                          (not (and (eq observer (slot-value txn 'source))
                                    (eq specimen (slot-value txn 'target))))))))
 
@@ -374,14 +423,17 @@
                   (lambda (node txn)
                     (if (eq observer (slot-value txn 'source))
                         (unless (eq specimen (slot-value txn 'target))
-                          (minusp (direct-favor node (slot-value txn 'target) *decay*)))
+                          (minusp (direct-favor node (slot-value txn 'target) *repeated-favor-decay*)))
                         (and (eq node (slot-value txn 'source))
                              (if (eq specimen (slot-value txn 'target))
                                  t
-                                 (plusp (direct-favor node (slot-value txn 'target) *decay*)))
+                                 (plusp (direct-favor node (slot-value txn 'target) *repeated-favor-decay*)))
                              (not (and (eq observer (slot-value txn 'source))
                                        (eq specimen (slot-value txn 'target)))))))))
 
+;;;
+;;; Testing
+;;;
 (defparameter *favors*
   '(("Opal" "Taraji" "Taraji" "veritas")
     ("Taraji" "Veritas" "Veritas" "Veritas")
