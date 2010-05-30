06b96e617ebfa0770602b6ebea876e248eb92641
Kat Marchán <kzm@sykosomatic.org>
2010-05-30 01:48:02 -0500

    Hello, World!

--- src/views.lisp
+++ src/views.lisp
@@ -1,3 +1,16 @@
 (in-package :ffavor)
 
+(defvar *title*)
 ;; web pages go here
+(defmacro with-template (title &body body)
+  `(yaclml:with-yaclml-output-to-string
+     (<:html
+      (<:head
+       (<:meta :http-equiv "Content-type" :content "text/html;charset=UTF-8")
+       (<:title ,title)
+       (<:link :rel "stylesheet" :type "text/css" :href "styles.css"))
+      (<:body ,@body))))
+
+(hunchentoot:define-easy-handler (home :uri "/") ()
+  (with-template "Home, Sweet Home"
+    (<:h1 "Hello, world!")))
