48323b87749f62be39efd267dd73e23f59652f65
Kat Marchán <kzm@sykosomatic.org>
2010-05-30 03:17:15 -0500

    Working on logins

--- src/views.lisp
+++ src/views.lisp
@@ -25,10 +25,32 @@
        ,@body)))
 
 (defhandler (home :uri "/") "Home, Sweet Home" ()
-  (<:h1 "Hello, world!"))
+  (if (and hunchentoot:*session* (hunchentoot:session-value 'username))
+      (<:h1 (<:ah (format nil "Hello, ~A!" (hunchentoot:session-value 'username))))
+      (progn
+        (hunchentoot:start-session)
+        (<:h1 "Welcome!")
+        (<:p "Please log in.")
+        (<:form :action "/login" :method "post"
+                (<:input :type "text" :name "username")
+                (<:input :type "text" :name "password")
+                (<:input :type "submit" :value "Log In")))))
 
-(defhandler (login :uri "/login") "Log In" ()
-  (<:p "Nothing to see here. Move along."))
+(defhandler (login :uri "/login") "Log In" (username password)
+  (when hunchentoot:*session*
+    (when (and username password)
+      (setf (hunchentoot:session-value 'username) username)
+      (<:p (<:ah (format nil "Tried to log on as '~A', using '~A' as your password." username password))))))
 
 (defhandler (favors :uri "/favors") "Check your favors" ()
-  (<:h1 "Various favors shall appear here!"))
+  (<:h1 "Various favors shall appear here!")
+  (when hunchentoot:*session*
+    (when (hunchentoot:session-value 'username)
+      (<:p (<:ah (format nil "Welcome, ~A!" (hunchentoot:session-value 'username))))
+      (let ((user (find-user (hunchentoot:session-value 'username))))
+        (when user
+          (<:p (<:ah (format nil "Current global favor: ~A"
+                             (coerce (global-favor user
+                                                   (query (:select (:min 'timestamp) :from 'transaction) :single)
+                                                   (query (:select (:max 'timestamp) :from 'transaction) :single))
+                                     'float)))))))))
