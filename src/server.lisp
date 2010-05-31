5417986d1b6026d42a0965e3ce02f1d7c5904345
Kat Marchán <kzm@sykosomatic.org>
2010-05-30 20:40:08 -0500

    Added theme and template 'support'

--- src/server.lisp
+++ src/server.lisp
@@ -2,4 +2,5 @@
 
 (defun start-server (db-name port)
   (connect-toplevel db-name "zkat" "" "localhost")
+  (push (create-folder-dispatcher-and-handler "/theme/" "theme/") *dispatch-table*)
   (start (make-instance 'acceptor :port port)))
